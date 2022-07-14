01d325e0d59d47db0c3f02129e695e8f87391296
BuTomer <bu.tomer@gmail.com>
2022-07-14 17:01:56 +0300

    step one - Formik Context Provider Wrapper We will remove the hook in a minute

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,5 +1,5 @@
 import styles from "./Login.module.scss";
-import { useFormik } from "formik";
+import { Form, Formik, useFormik } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
@@ -34,52 +34,61 @@ const Login = () => {
   });
 
   return (
-    <form onSubmit={formik.handleSubmit} className={styles["login-form"]}>
-      <section className={styles["form-group"]}>
-        <label htmlFor="username">User Name</label>
+    <Formik
+      validationSchema={validationSchema}
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+    >
+      <form onSubmit={formik.handleSubmit} className={styles["login-form"]}>
+        <section className={styles["form-group"]}>
+          <label htmlFor="username">User Name</label>
+          <input
+            autoComplete="username"
+            {...formik.getFieldProps("username")}
+            type="text"
+            name="username"
+            id="username"
+          />
+
+          {formik.touched.username && formik.errors.username && (
+            <div>{formik.errors.username}</div>
+          )}
+        </section>
+
+        <section className={styles["form-group"]}>
+          <label htmlFor="email">User Email</label>
+          <input
+            autoComplete="email"
+            {...formik.getFieldProps("email")}
+            type="email"
+            name="email"
+            id="email"
+          />
+          {formik.touched.email && formik.errors.email && (
+            <div>{formik.errors.email}.</div>
+          )}
+        </section>
+
+        <section className={styles["form-group"]}>
+          <label htmlFor="password">Password</label>
+          <input
+            autoComplete="current-password"
+            {...formik.getFieldProps("password")}
+            type="password"
+            name="password"
+            id="password"
+          />
+          {formik.touched.password && formik.errors.password && (
+            <div>{formik.errors.password}</div>
+          )}
+        </section>
+
         <input
-          autoComplete="username"
-          {...formik.getFieldProps("username")}
-          type="text"
-          name="username"
-          id="username"
+          className={`${styles.btn} btn btn-lg btn-danger`}
+          type="submit"
         />
-
-        {formik.touched.username && formik.errors.username && (
-          <div>{formik.errors.username}</div>
-        )}
-      </section>
-
-      <section className={styles["form-group"]}>
-        <label htmlFor="email">User Email</label>
-        <input
-          autoComplete="email"
-          {...formik.getFieldProps("email")}
-          type="email"
-          name="email"
-          id="email"
-        />
-        {formik.touched.email && formik.errors.email && (
-          <div>{formik.errors.email}.</div>
-        )}
-      </section>
-
-      <section className={styles["form-group"]}>
-        <label htmlFor="password">Password</label>
-        <input
-          autoComplete="current-password"
-          {...formik.getFieldProps("password")}
-          type="password"
-          name="password"
-          id="password"
-        />
-        {formik.touched.password && formik.errors.password && (
-          <div>{formik.errors.password}</div>
-        )}
-      </section>
-
-      <input className={`${styles.btn} btn btn-lg btn-danger`} type="submit" />
-    </form>
+      </form>
+    </Formik>
   );
 };
 
